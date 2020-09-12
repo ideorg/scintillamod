e67e13b6cca7f3539e0d27362f1bcd24c992bfb9
Andrzej Borucki <borucki.andrzej@gmail.com>
2020-09-13 00:49:44 +0200

    lexers from ver3, but not works

--- main.cpp
+++ main.cpp
@@ -31,6 +31,110 @@ bool MyApp::OnInit()
           frame->Show(true);
     return true;
 }
+
+void SetEditorStyle(wxStyledTextCtrl* stc)
+{
+    // Porządki
+    stc->StyleClearAll();
+
+    // NUMEROWANIE LINII
+    stc->SetMarginWidth(0, 30);
+    stc->SetMarginType(0, wxSTC_MARGIN_NUMBER);
+    stc->StyleSetForeground(wxSTC_STYLE_LINENUMBER, wxColour(155, 255, 255));
+    stc->StyleSetBackground(wxSTC_STYLE_LINENUMBER, wxColour(0, 128, 255));
+
+    stc->SetWrapMode(0);
+
+    stc->SetLexer(wxSTC_LEX_CPP);
+
+    wxFont font(10, wxSWISS, wxNORMAL, wxNORMAL, false, "Courier New");
+    wxFont fontb(10, wxSWISS, wxNORMAL, wxFONTWEIGHT_BOLD, false, "Courier New");
+
+    stc->StyleSetFont(wxSTC_C_DEFAULT, font);
+    stc->StyleSetFont(wxSTC_C_COMMENT, font);
+    stc->StyleSetFont(wxSTC_C_COMMENTLINE, font);
+    stc->StyleSetFont(wxSTC_C_COMMENTDOC, font);
+    stc->StyleSetFont(wxSTC_C_NUMBER, font);
+    stc->StyleSetFont(wxSTC_C_WORD, fontb);
+    stc->StyleSetFont(wxSTC_C_STRING, font);
+    stc->StyleSetFont(wxSTC_C_CHARACTER, font);
+    stc->StyleSetFont(wxSTC_C_UUID, font);
+    stc->StyleSetFont(wxSTC_C_PREPROCESSOR, font);
+    stc->StyleSetFont(wxSTC_C_OPERATOR, font);
+    stc->StyleSetFont(wxSTC_C_IDENTIFIER, font);
+    stc->StyleSetFont(wxSTC_C_STRINGEOL, font);
+    stc->StyleSetFont(wxSTC_C_VERBATIM, font);
+    stc->StyleSetFont(wxSTC_C_REGEX, font);
+    stc->StyleSetFont(wxSTC_C_COMMENTLINEDOC, font);
+    stc->StyleSetFont(wxSTC_C_WORD2, fontb);
+    stc->StyleSetFont(wxSTC_C_COMMENTDOCKEYWORD, font);
+    stc->StyleSetFont(wxSTC_C_COMMENTDOCKEYWORDERROR, font);
+    stc->StyleSetFont(wxSTC_C_GLOBALCLASS, font);
+    stc->StyleSetFont(wxSTC_C_STRINGRAW, font);
+    stc->StyleSetFont(wxSTC_C_TRIPLEVERBATIM, font);
+    stc->StyleSetFont(wxSTC_C_HASHQUOTEDSTRING, font);
+    stc->StyleSetFont(wxSTC_C_PREPROCESSORCOMMENT, font);
+
+    stc->StyleSetForeground(wxSTC_C_STRING, wxColour(255, 0, 0));
+    stc->StyleSetForeground(wxSTC_C_PREPROCESSOR, wxColour(0, 128, 0));
+    stc->StyleSetForeground(wxSTC_C_IDENTIFIER, wxColour(0, 0, 0));
+    stc->StyleSetForeground(wxSTC_C_NUMBER, wxColour(128, 0, 128));
+    stc->StyleSetForeground(wxSTC_C_CHARACTER, wxColour(150, 0, 0));
+    stc->StyleSetForeground(wxSTC_C_WORD, wxColour(0, 64, 128));
+    stc->StyleSetForeground(wxSTC_C_WORD2, wxColour(0, 128, 128));
+    stc->StyleSetForeground(wxSTC_C_COMMENT, wxColour(0, 128, 192));
+    stc->StyleSetForeground(wxSTC_C_COMMENTLINE, wxColour(0, 128, 192));
+    stc->StyleSetForeground(wxSTC_C_COMMENTDOC, wxColour(0, 128, 192));
+    stc->StyleSetForeground(wxSTC_C_COMMENTDOCKEYWORD, wxColour(0, 128, 192));
+    stc->StyleSetForeground(wxSTC_C_COMMENTDOCKEYWORDERROR, wxColour(0, 128, 192));
+
+    stc->StyleSetBold(wxSTC_C_WORD, true);
+    stc->StyleSetBold(wxSTC_C_WORD2, true);
+    stc->StyleSetBold(wxSTC_C_COMMENTDOCKEYWORD, true);
+
+    // SKŁADANIE KODU
+    stc->SetMarginWidth(1, 16);
+    stc->SetMarginType(1, wxSTC_MARGIN_SYMBOL);
+    stc->SetMarginMask(1, wxSTC_MASK_FOLDERS);
+    stc->StyleSetBackground(1, wxColor(200, 200, 200));
+    stc->SetMarginSensitive(1, true);
+
+    stc->SetProperty(wxT("fold"), wxT("1"));
+
+    wxColour custom(100, 100, 100);
+    // zwinięty blok główny
+    stc->MarkerDefine(wxSTC_MARKNUM_FOLDER, wxSTC_MARK_BOXPLUS);
+    stc->MarkerSetForeground(wxSTC_MARKNUM_FOLDER, *wxWHITE);
+    stc->MarkerSetBackground(wxSTC_MARKNUM_FOLDER, custom);
+
+    // rozwinięty blok główny
+    stc->MarkerDefine(wxSTC_MARKNUM_FOLDEROPEN, wxSTC_MARK_BOXMINUS);
+    stc->MarkerSetForeground(wxSTC_MARKNUM_FOLDEROPEN, *wxWHITE);
+    stc->MarkerSetBackground(wxSTC_MARKNUM_FOLDEROPEN, custom);
+
+    // koniec bloku głównego
+    stc->MarkerDefine(wxSTC_MARKNUM_FOLDERTAIL, wxSTC_MARK_LCORNER);
+    stc->MarkerSetBackground(wxSTC_MARKNUM_FOLDERTAIL, custom);
+
+    // zwinięty blok wewnętrzny
+    stc->MarkerDefine(wxSTC_MARKNUM_FOLDEREND, wxSTC_MARK_BOXPLUSCONNECTED);
+    stc->MarkerSetForeground(wxSTC_MARKNUM_FOLDEREND, *wxWHITE);
+    stc->MarkerSetBackground(wxSTC_MARKNUM_FOLDEREND, custom);
+
+    // rozwinięty blok wewnętrzny
+    stc->MarkerDefine(wxSTC_MARKNUM_FOLDEROPENMID, wxSTC_MARK_BOXMINUSCONNECTED);
+    stc->MarkerSetForeground(wxSTC_MARKNUM_FOLDEROPENMID, *wxWHITE);
+    stc->MarkerSetBackground(wxSTC_MARKNUM_FOLDEROPENMID, custom);
+
+    // koniec bloku wewnętrznego
+    stc->MarkerDefine(wxSTC_MARKNUM_FOLDERMIDTAIL, wxSTC_MARK_TCORNER);
+    stc->MarkerSetBackground(wxSTC_MARKNUM_FOLDERMIDTAIL, custom);
+
+    // treść bloku
+    stc->MarkerDefine(wxSTC_MARKNUM_FOLDERSUB, wxSTC_MARK_VLINE);
+    stc->MarkerSetBackground(wxSTC_MARKNUM_FOLDERSUB, custom);
+}
+
 MyFrame::MyFrame()
         : wxFrame(NULL, wxID_ANY, "Hello World")
 {
@@ -50,11 +154,17 @@ MyFrame::MyFrame()
     Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
-    wxStyledTextCtrl* stc = new wxStyledTextCtrl(this, 1, wxDefaultPosition, wxDefaultSize, 0L, "");
+    wxStyledTextCtrl* stc = new wxStyledTextCtrl(this, 1, wxDefaultPosition, wxDefaultSize, 0L, "main.cpp");
     wxBoxSizer *mainSizer = new wxBoxSizer(wxVERTICAL);
     this->SetSizer(mainSizer);
     mainSizer->Add(stc, 1, wxEXPAND | wxALL, 0);
-    stc->Paste();
+    /*stc->StyleClearAll();
+    stc->SetLexer(wxSTC_LEX_CPP);
+    stc->StyleSetForeground(wxSTC_C_COMMENT, wxColour(0, 128, 192));
+    stc->StyleSetForeground(wxSTC_C_NUMBER, wxColour(128, 0, 128));*/
+
+    SetEditorStyle(stc);
+    stc->LoadFile("../main.cpp");
 }
 void MyFrame::OnExit(wxCommandEvent& event)
 {
