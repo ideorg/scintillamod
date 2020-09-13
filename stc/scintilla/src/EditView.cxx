e6b1a25c54ec14c8368116a7ab10f7ac36e2d7fb
Andrzej Borucki <borucki.andrzej@gmail.com>
2020-09-13 21:54:07 +0200

    using second from vector pair

--- stc/scintilla/src/EditView.cxx
+++ stc/scintilla/src/EditView.cxx
@@ -1416,7 +1416,7 @@ void EditView::DrawInterAnnotationText(Surface *surface, const EditModel &model,
     const XYPOSITION virtualSpace = model.sel.VirtualSpaceFor(
             model.pdoc->LineEnd(line)) * spaceWidth;
     rcSegment.left = xStart +
-                     static_cast<XYPOSITION>(ll->positions[std::min(ll->numCharsInLine,2)] - subLineStart)
+                     static_cast<XYPOSITION>(ll->positions[std::min(ll->numCharsInLine,interStruct->v[0].second-1)] - subLineStart)
                      + virtualSpace + vsDraw.aveCharWidth;
 
     const char *textFoldDisplay = model.GetFoldDisplayText(line);
